4619ac83d4a1c124ec6ce7c9eb52170aaa313f60
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-02-24 22:53:13 -0500

    added test classes and cleaned up unused code

--- src/edu/temple/cis/c3238/banksim/Bank.java
+++ src/edu/temple/cis/c3238/banksim/Bank.java
@@ -1,8 +1,5 @@
 package edu.temple.cis.c3238.banksim;
 
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -12,7 +9,6 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author Modified by Alexa Delacenserie
  * @author Modified by Tarek Elseify
  */
-
 public class Bank {
 
     public static final int NTEST = 10;
@@ -20,10 +16,8 @@ public class Bank {
     private long numTransactions = 0;
     private final int initialBalance;
     private final int numAccounts;
-    private CyclicBarrier barrier;
-    Runnable testingThread;
     private boolean open = true;
-    AtomicInteger atom;
+    AtomicInteger atom;             // use an atomic integer to provide mutual exclusion for test method
 
     public Bank(int numAccounts, int initialBalance) {
         this.initialBalance = initialBalance;
@@ -33,80 +27,93 @@ public class Bank {
             accounts[i] = new Account(this, i, initialBalance);
         }
         numTransactions = 0;
-    	testingThread = new TestingThread(accounts, numAccounts, initialBalance);
-    	barrier = new CyclicBarrier(numAccounts, testingThread);
-    	atom = new AtomicInteger(0);
+        // use atomic int to make sure that
+        atom = new AtomicInteger(0);
     }
 
     public void transfer(int from, int to, int amount) {
-    	
+
+        //call function to check if bank needs to wait for more funds
         accounts[from].waitForAvailableFunds(amount);
-    	atom.incrementAndGet();
+        // if we are transfering then the atomic int will not be 0
+        increment();
         if (accounts[from].withdraw(amount)) {
             accounts[to].deposit(amount);
         }
-        atom.decrementAndGet();
+        decrement();
         Thread.yield();
+        // once we leave the transfer let the test know it can test.
         // Uncomment line when race condition in test() is fixed.
-        if (shouldTest()) test();
+
+            if (shouldTest()) {
+                test();
+            }
+
     }
 
     public synchronized void test() {
-/*    	try {
-			barrier.await();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (BrokenBarrierException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-    	while ((atom.get() != 0) && isOpen()){
-    		try {
-				wait(10);
-				//System.out.printf("QWERTY %d\n", atom.get());
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-    	}
-		System.out.printf("atom is %d\n", atom.get());
-		if (atom.get() == 0) {
-			System.out.printf("atom is %d\n", atom.get());
-	    	int totalBalance = 0;
-	        for (Account account : accounts) {
-	            System.out.printf("%-30s %s%n", 
-	                    Thread.currentThread().toString(), account.toString());
-	            totalBalance += account.getBalance();
-	        }
-	        System.out.printf("%-30s Total balance: %d\n", Thread.currentThread().toString(), totalBalance);
-	        if (totalBalance != numAccounts * initialBalance) {
-	            System.out.printf("%-30s Total balance changed!\n", Thread.currentThread().toString());
-	            System.exit(0);
-	        } else {
-	            System.out.printf("%-30s Total balance unchanged.\n", Thread.currentThread().toString());
-	        }
-		}
-			//testingThread.run();
-		notifyAll();
+
+        while ((atom.get() != 0) && isOpen()) {
+            try {
+                wait(10);
+                //System.out.printf("QWERTY %d\n", atom.get());
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+        }
+        // keep track of atomic int for debugging
+       // System.out.printf("atom is %d\n", atom.get());
+        if (atom.get() == 0) {
+         //   System.out.printf("atom is %d\n", atom.get());
+            int totalBalance = 0;
+            for (Account account : accounts) {
+                System.out.printf("%-30s %s%n",
+                        Thread.currentThread().toString(), account.toString());
+                totalBalance += account.getBalance();
+            }
+            System.out.printf("%-30s Total balance: %d\n", Thread.currentThread().toString(), totalBalance);
+            if (totalBalance != numAccounts * initialBalance) {
+                System.out.printf("%-30s Total balance changed!\n", Thread.currentThread().toString());
+                System.exit(0);
+            } else {
+                System.out.printf("%-30s Total balance unchanged.\n", Thread.currentThread().toString());
+            }
+        }
+        // wake any threads that are sleeping. ( should be none)
+        notifyAll();
     }
 
     public int getNumAccounts() {
         return numAccounts;
     }
-    
-    public boolean shouldTest() {
+
+    public synchronized boolean shouldTest() {
         return ++numTransactions % NTEST == 0;
     }
-    
-    synchronized boolean isOpen() {return open;}
-    
+
+    synchronized boolean isOpen() {
+        return open;
+    }
+
+    synchronized void increment(){
+
+        atom.incrementAndGet();
+    }
+
+    synchronized void decrement(){
+
+        atom.decrementAndGet();
+    }
+
     void closeBank() {
         synchronized (this) {
             open = false;
         }
         for (Account account : accounts) {
-            synchronized (account) {account.notifyAll();}
+            synchronized (account) {
+                account.notifyAll();
+            }
         }
     }
 }
--- src/test/java/accountTest.java
+++ src/test/java/accountTest.java
@@ -4,22 +4,57 @@ in the banking sim for lab 5 3296 at Temple university.
  */
 package test.java;
 
+import edu.temple.cis.c3238.banksim.Account;
+import edu.temple.cis.c3238.banksim.Bank;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 /**
  *
  * @author brahm
  */
 public class accountTest {
 
+    private int initialBalance;
+    private int numAccounts;
+
+    private Bank bank;
+    private Account accounts[];
+    // TODO intialization for tests
+    @Before
+    public void init() {
+        this.initialBalance = 10000;
+        this.numAccounts = 10;
+
+        this.bank = new Bank(this.numAccounts, this.initialBalance);
+        this.accounts = this.bank.accounts;
+    }
     
-    // intialization for tests
-    
-    
-    // test to withdraw some amount then check balance is correct
-    
-    
-    // test to deposit some amount then check that balance is correct
-    
-    
-    // test that account starts with correct amount
-    
+    // TODO test to withdraw some amount then check balance is correct
+
+    @Test
+    public void Withdraw2000ThenCheckBalance() {
+        boolean shouldBeTrue = this.accounts[0].withdraw(2000);
+        assertEquals("The withdraw function should return true.",
+                shouldBeTrue,
+                true);
+        assertEquals("The account balance should be 9,000.",
+                this.accounts[0].getBalance(),
+                8000);
+    }
+
+    // TODO test to deposit some amount then check that balance is correct
+
+    @Test public void ShouldDeposit2000ThenCheckBalance(){
+
+        this.accounts[1].deposit(2000);
+        int amount = this.accounts[1].getBalance();
+
+        assertEquals("the balance should be 12000",
+                this.accounts[1].getBalance(),
+                12000);
+
+    }
+
 }
